4ae4b22d2f3161ca81ab802136263a280bd8b20f
Ayoub-benkhedher <80359891+Ayoub-benkhedher@users.noreply.github.com>
2021-07-05 14:16:47 -0500

    ,,;'''

--- BoldGame/MainWindow.xaml.cs
+++ BoldGame/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace BoldGame
     {
         int x = 0;
         int y = 0;
+        int cordx = 0;
+        int cordy = 0;
 
         public MainWindow()
         {
@@ -31,7 +33,14 @@ namespace BoldGame
 
         private void OnRendering(object sender, EventArgs e)
         {
+            string coordinations;
             DebugTextBlock.Text = Mouse.GetPosition(DebugTextBlock).ToString();
+            coordinations = DebugTextBlock.Text;
+            string[] xny = coordinations.Split(',');
+            foreach(var cord in xny)
+            {
+                Console.WriteLine($"<{cord}>");
+            }
         }
         private void CreateViewImageDynamically(int x, int y)
         {
